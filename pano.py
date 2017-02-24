0f39cf97079f52c711df45e6e88e6c704303b2e5
= <kate.tolstaya@gmail.com>
2017-02-24 17:06:08 -0500

    pano blending

--- pano.py
+++ pano.py
@@ -2,11 +2,13 @@ import numpy as np
 from scipy import io
 from Quaternion import *
 import matplotlib.pyplot as plt
+from PIL import Image
+from PIL import ImageDraw
 import cv2
 #from findDimensions import findDimensions
 import math
 file_num = 8
-mydata = 1 # flag decides whether to use vicon or my filtered data
+mydata = 0 # flag decides whether to use vicon or my filtered data
 
 if mydata:
     fileName = "filtered" + str(file_num) + ".npy"
@@ -21,31 +23,16 @@ else:
 camdata = io.loadmat('cam/cam' + str(file_num) + '.mat')
 pics = camdata['cam']  # dimensions (240, 320, 3, 1685)
 pics_t = camdata['ts']  # dimensions (1, 1685)]
-
-
 im_size = tuple(np.shape(pics[:,:,:,0]))
 h_pix = im_size[0]
 w_pix = im_size[1]
+
+# set up pano canvas
 pano_size = (1000,1000,3)
-
-#result = np.zeros(pano_size, np.uint8)
-
-pano = np.zeros(pano_size)
-#
-
-# #x = np.linspace(0, 1 , 240)
-# #y = np.linspace(0, 1,320)
-# #xv, yv = np.meshgrid(y, x)
-# #xv = np.reshape(xv,(np.shape(xv)[0], np.shape(xv)[1],1))
-# #yv = np.reshape(yv,(np.shape(yv)[0], np.shape(yv)[1],1))
-#
-# f = 1 # focal point??
-# #zv = np.tile(f, (np.shape(xv)[0], np.shape(xv)[1],1))
-# #coords = np.concatenate((xv,yv,zv), axis=2)
+pano = np.zeros(pano_size, dtype=np.uint8)
 
 
-T = 500 #np.shape(pics_t)[1]
-
+# pre-calculate arrays to be used for pixel rotations
 h_fov = np.radians(45)
 w_fov = np.radians(60)
 
@@ -56,12 +43,13 @@ yv, xv = np.meshgrid(y_u, x_v, sparse=False, indexing='ij')
 lam = -(y_u - h_pix/2) / h_pix * h_fov
 phi = -(x_v - w_pix/2) / w_pix * w_fov
 
-x = np.outer(np.cos(lam),np.cos(phi))
-y = np.outer(np.cos(lam),np.sin(phi))
+x = np.outer(np.cos(lam), np.cos(phi))
+y = np.outer(np.cos(lam), np.sin(phi))
 z = np.tile(np.sin(lam).reshape(-1,1),(1,w_pix))
 cart = np.dstack((x,y,z))
 
-for t in range(0, T,10):
+T = np.shape(pics_t)[1]
+for t in range(0, T,5):
     i = np.argmin(q_t <= pics_t[0, t])
     if mydata:
         R = q[i].to_rotation()
@@ -79,64 +67,82 @@ for t in range(0, T,10):
     Y = r_cart[:, :, 1]
     Z = r_cart[:, :, 2]
 
-
     pitch = np.arccos(Z / np.sqrt(np.power(X,2) + np.power(Y,2) + np.power(Z,2)))
     yaw = np.arctan2(X,Y) + np.pi
     h = (pitch / math.pi * pano_size[0]).astype(int)
     theta = (yaw / math.pi / 2 * pano_size[1]).astype(int)
 
+    ################ blending
+    empty_pano = np.zeros(pano_size, dtype=np.uint8)
+    empty_pano[h.tolist(), theta.tolist(), :] = frame
+
+    # get the intersecting region
+    #ret1, mask1 = cv2.threshold(cv2.cvtColor(empty_pano, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+    ret2, mask2 = cv2.threshold(cv2.cvtColor(pano, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+    #mask = np.bitwise_and(mask1, mask2)
+
+    # empty pano has old pano on top
+    empty_pano = cv2.add(empty_pano, pano, mask=mask2)
+
+    # pano has the newest frame on top
     pano[h.tolist(), theta.tolist(), :] = frame
 
-    # for i in range(0, h_pix):
-    #     for j in range(0, w_pix):
-    #         r_coords = np.dot(R, np.array([x[i,j], y[i,j], z[i,j]]))
-    #
-    #         X = r_coords[0]
-    #         Y = r_coords[1]
-    #         Z = r_coords[2]
-    #
-    #         lam2 = np.arccos(Z / np.sqrt(X**2 + Y**2 + Z**2))
-    #         phi2 = np.arctan(Y/X)
-    #
-    #         #theta = np.mod(int((np.arctan(Y / X) - np.pi) / (2 * np.pi) * res_theta + res_theta / 2), res_theta)
-    #         #h = np.mod(int(Z / np.sqrt(X ** 2 + Y ** 2) * res_h * 10), res_h)
-    #         theta = int(lam2 / math.pi / 2 * pano_size[1])
-    #         h = int((phi2+math.pi/2) / math.pi * pano_size[0])
-    #         pano[h, theta, :] = frame[i, j, :]
+    pano = cv2.addWeighted(empty_pano, 0.5, pano, 0.5, 0)
 
-
-
-    # for y_u in range(1, 240):
-    #     for x_v in range(1, 320):
-    #
-    #         lam = (y_u) / h_pix * h_fov
-    #         phi = (x_v) / w_pix * w_fov
-    #
-    #         # the order of x,y,z and phi, lam is incorrect
-    #         x = np.cos(lam)*np.cos(phi)
-    #         y = np.cos(lam)*np.sin(phi)
-    #         z = np.sin(lam)
-    #
-    #         r_coords = np.dot(R, np.array([x, y, z]))
-    #
-    #         X = r_coords[0]
-    #         Y = r_coords[1]
-    #         Z = r_coords[2]
-    #         # mistakes end here
-    #
-    #         lam2 = np.arccos(Z / np.sqrt(X**2 + Y**2 + Z**2))
-    #         phi2 = np.arctan(Y/X)
-    #
-    #         #theta = np.mod(int((np.arctan(Y / X) - np.pi) / (2 * np.pi) * res_theta + res_theta / 2), res_theta)
-    #         #h = np.mod(int(Z / np.sqrt(X ** 2 + Y ** 2) * res_h * 10), res_h)
-    #         theta = int(lam2 / math.pi / 2 * pano_size[1])
-    #         h = int(phi2 / math.pi * pano_size[0])
-    #         pano[h, theta, :] = frame[y_u, x_v, :]
-            #count += 1
 
 # ip = interp2d(x, y, z); zi = ip(xi, yi)
+#pano = cv2.cvtColor(pano, cv2.COLOR_BGR2RGB)
 plt.imshow(pano)
 plt.show()
+
+################################################################################### OLD
+
+# for i in range(0, h_pix):
+#     for j in range(0, w_pix):
+#         r_coords = np.dot(R, np.array([x[i,j], y[i,j], z[i,j]]))
+#
+#         X = r_coords[0]
+#         Y = r_coords[1]
+#         Z = r_coords[2]
+#
+#         lam2 = np.arccos(Z / np.sqrt(X**2 + Y**2 + Z**2))
+#         phi2 = np.arctan(Y/X)
+#
+#         #theta = np.mod(int((np.arctan(Y / X) - np.pi) / (2 * np.pi) * res_theta + res_theta / 2), res_theta)
+#         #h = np.mod(int(Z / np.sqrt(X ** 2 + Y ** 2) * res_h * 10), res_h)
+#         theta = int(lam2 / math.pi / 2 * pano_size[1])
+#         h = int((phi2+math.pi/2) / math.pi * pano_size[0])
+#         pano[h, theta, :] = frame[i, j, :]
+
+
+
+# for y_u in range(1, 240):
+#     for x_v in range(1, 320):
+#
+#         lam = (y_u) / h_pix * h_fov
+#         phi = (x_v) / w_pix * w_fov
+#
+#         # the order of x,y,z and phi, lam is incorrect
+#         x = np.cos(lam)*np.cos(phi)
+#         y = np.cos(lam)*np.sin(phi)
+#         z = np.sin(lam)
+#
+#         r_coords = np.dot(R, np.array([x, y, z]))
+#
+#         X = r_coords[0]
+#         Y = r_coords[1]
+#         Z = r_coords[2]
+#         # mistakes end here
+#
+#         lam2 = np.arccos(Z / np.sqrt(X**2 + Y**2 + Z**2))
+#         phi2 = np.arctan(Y/X)
+#
+#         #theta = np.mod(int((np.arctan(Y / X) - np.pi) / (2 * np.pi) * res_theta + res_theta / 2), res_theta)
+#         #h = np.mod(int(Z / np.sqrt(X ** 2 + Y ** 2) * res_h * 10), res_h)
+#         theta = int(lam2 / math.pi / 2 * pano_size[1])
+#         h = int(phi2 / math.pi * pano_size[0])
+#         pano[h, theta, :] = frame[y_u, x_v, :]
+# count += 1
 
         #next_img_warp = cv2.warpPerspective(pics[:, :, :, t], np.eye(3), (pano_size[1], pano_size[0]))
 
